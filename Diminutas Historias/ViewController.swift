f1c296f22f0a984d2dbe2e3267c03311099dd502
Maestro <Maestro@imac-de-maestro.local>
2017-02-09 10:18:36 -0700

    Initial Commit

---
+++ Diminutas Historias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diminutas Historias
+//
+//  Created by Maestro on 09/02/17.
+//  Copyright © 2017 DreamTeamCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
